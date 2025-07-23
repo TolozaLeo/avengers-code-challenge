a26d45cf892a779fac9bf02bc55ac03719a5226c
TolozaLeo <leo_eltolo@hotmail.com>
2025-07-23 19:10:04 -0300

    Added: Color Theme

--- app/src/main/java/dev/leotoloza/avengersapp/ui/theme/Color.kt
+++ app/src/main/java/dev/leotoloza/avengersapp/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package dev.leotoloza.avengersapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val White = Color(0xFFFFFFFF)
+val Gray = Color(0xFFECEFF1)
+val DarkGrey = Color(0xFF262626)
+val Title = Color(0xFF000000)
+val Subtitle = Color(0xFF00000D)
+val Description = Color(0xFF222053)
