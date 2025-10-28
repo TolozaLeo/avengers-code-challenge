f210a60e0087f8c85256ed1cf6164eaa730e24d0
TolozaLeo <leo_eltolo@hotmail.com>
2025-10-27 22:58:52 -0300

    Refactor: Se refactorizó la generación de `timestamp` y `hash` para las llamadas a la API.
    - Se eliminó la constante `TIME_STAMP` de `AvengersClient.kt`.
    - En `AvengerApiService.kt`, ahora se genera un nuevo `timestamp` y `hash` para cada llamada a `getCharacters` y `getEvents`, asegurando la validez de cada petición.
    - Se agregó un comentario en `AvengersClient.kt` para documentar un comportamiento inesperado de la API de eventos.

--- app/src/main/java/dev/leotoloza/avengersapp/data/service/AvengerApiService.kt
+++ app/src/main/java/dev/leotoloza/avengersapp/data/service/AvengerApiService.kt
@@ -2,28 +2,40 @@ package dev.leotoloza.avengersapp.data.service
 
 import dev.leotoloza.avengersapp.BuildConfig
 import dev.leotoloza.avengersapp.data.service.security.HashKeyBuilder
+import java.util.Date
 import javax.inject.Inject
 
 class AvengerApiService @Inject constructor(
     private val apiClient: AvengersClient,
-    hashKeyBuilder: HashKeyBuilder,
+    private val hashKeyBuilder: HashKeyBuilder,
 ) {
-    private val hashKey = hashKeyBuilder.build(
-        timestamp = TIME_STAMP.toString(),
-        privateKey = BuildConfig.API_PRIVATE_KEY,
-        publicKey = BuildConfig.API_PUBLIC_KEY
-    )
     private val publicKey = BuildConfig.API_PUBLIC_KEY
+    private val privateKey = BuildConfig.API_PRIVATE_KEY
 
-    suspend fun getCharacters(page: Int) = apiClient.getCharacters(
-        publicApiKey = publicKey,
-        hash = hashKey,
-        offset = (page * CHARACTERS_PER_PAGE),
-    )
+    // Genera ts y hash nuevos para cada llamada
+    suspend fun getCharacters(page: Int) = run {
+        val ts = Date().time.toString()
+        val hash = hashKeyBuilder.build(
+            timestamp = ts, privateKey = privateKey, publicKey = publicKey
+        )
+        apiClient.getCharacters(
+            ts = ts.toLong(), // Pasa el ts nuevo
+            publicApiKey = publicKey,
+            hash = hash, // Pasa el hash nuevo
+            offset = (page * CHARACTERS_PER_PAGE),
+        )
+    }
 
-    suspend fun getEvents(page: Int) = apiClient.getEvents(
-        publicApiKey = publicKey,
-        hash = hashKey,
-        offset = (page * EVENTS_PER_PAGE),
-    )
+    suspend fun getEvents(page: Int) = run {
+        val ts = Date().time.toString()
+        val hash = hashKeyBuilder.build(
+            timestamp = ts, privateKey = privateKey, publicKey = publicKey
+        )
+        apiClient.getEvents(
+            ts = ts.toLong(), // Pasa el ts nuevo
+            publicApiKey = publicKey,
+            hash = hash, // Pasa el hash nuevo
+            offset = (page * EVENTS_PER_PAGE),
+        )
+    }
 }
--- app/src/main/java/dev/leotoloza/avengersapp/data/service/AvengersClient.kt
+++ app/src/main/java/dev/leotoloza/avengersapp/data/service/AvengersClient.kt
@@ -5,30 +5,31 @@ import dev.leotoloza.avengersapp.data.remote.model.EventsResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-const val TIME_STAMP = 1L
-
 const val CHARACTERS_PER_PAGE = 15
 
+/*Por algún motivo cuando pido más de 12 eventos o cuando los ordeno "-startDate"
+ la api devuelve código 200(respuesta ok), pero la respuesta es un string vacío.
+ La app lanza una excepcion por tener todos los campos esperados como nulos. */
 const val EVENTS_PER_PAGE = 12
-//const val START_DATE = "-startDate" //Ordena de mas reciente a mas antiguo
+//const val START_DATE = "-startDate" //Ordena de más reciente a más antiguo
 
 interface AvengersClient {
     @GET("characters")
     suspend fun getCharacters(
-        @Query("ts") ts: Long = TIME_STAMP,
+        @Query("ts") ts: Long,
         @Query("apikey") publicApiKey: String,
         @Query("hash") hash: String,
         @Query("offset") offset: Int,// a partir de que elemento se pide
         @Query("limit") limit: Int = CHARACTERS_PER_PAGE,
-    ) : CharactersResponse
+    ): CharactersResponse
 
     @GET("events")
     suspend fun getEvents(
-        @Query("ts") ts: Long = TIME_STAMP,
+        @Query("ts") ts: Long,
         @Query("apikey") publicApiKey: String,
         @Query("hash") hash: String,
         @Query("offset") offset: Int,
         @Query("limit") limit: Int = EVENTS_PER_PAGE,
 //        @Query("orderBy") orderBy: String = START_DATE
-    ) : EventsResponse
+    ): EventsResponse
 }
